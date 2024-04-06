8d502fd60d4d859ee62d0a0fc9e3ca0d4e5de55d
rachel-pu <rpu@ufl.edu>
2024-04-06 14:22:15 -0400

    moved js

--- static/javascript/resetInterview.js
+++ static/javascript/resetInterview.js
@@ -1,12 +1,11 @@
-//    const resetButton = document.getElementById('reset-button');
-//
-//    document.addEventListener('DOMContentLoaded', function() {
-//        resetButton.addEventListener('click', function() {
-//            fetch('/clear_array', {
-//                method: 'POST',
-//            })
-//                .then(response => response.json())
-//                .then(data => alert(data.message))
-//                .catch(error => console.error('Error:', error));
-//        });
-//    });
+document.addEventListener('DOMContentLoaded', function() {
+        const resetButton = document.getElementById('reset-button');
+        resetButton.addEventListener('click', function() {
+            fetch('/clear_array', {
+                method: 'POST',
+            })
+            .then(response => response.json())
+            .then(data => alert(data.message))
+            .catch(error => console.error('Error:', error));
+        });
+    });
