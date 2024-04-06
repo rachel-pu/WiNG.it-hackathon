cb18803bd4fe4ee9ddcca7919feea92122bfcfa7
xiaoguo-jia <xiaoguojia@ufl.edu>
2024-04-06 03:15:37 -0400

    new files
    earaly commit so i can pull the csv from rachel, dont look at this

--- response.py
+++ response.py
@@ -3,40 +3,32 @@ import random
 import time
 import speech_recognition as sr
 from collections import Counter
+import pandas as pd
 
 class InterviewResponse:
-    def __init__(self, questions):
-        self.questions = questions
-        self.responses = ["" for _ in range(5)]
-        self.response_times = [None for _ in range(5)]
-        self.filler_words_counts = [None for _ in range(5)]
-        self.most_repeated_words_lists = [None for _ in range(5)]
+    
+    # each object will be a question and traits
+    
+    def __init__(self, question):
+        self.question = question                    # question 
+        self.response = ""                          # answer recorded from the audio
+        self.response_time = None                   # will be recorded with timer?
+        self.filler_count = None                    # to be aggregated later
+        self.most_repeated_words = None             # for each question?
+        self.tone = None
 
-    def record_response(self, question_index, response, response_time):
-        self.responses[question_index] = response
-        self.response_times[question_index] = response_time
+    def word_count(self):
 
-    def analyze_responses(self):
-        for i, response in enumerate(self.responses):
-            words = response.split()
-            word_count = Counter(words)
-            self.most_repeated_words_lists[i] = word_count.most_common(5)
+        words = response.split()
+        word_count = Counter(words)
+        self.most_repeated_words = word_count.most_common(5)        # for each question
 
-            fillers = ["um", "uh", "like", "so"]
-            self.filler_words_counts[i] = sum(word_count.get(filler, 0) for filler in fillers)
+        fillers = ["um", "uh", "like", "so"]
+        self.filler_count = sum(word_count.get(filler, 0) for filler in fillers)
+    
 
-    def get_responses_data(self):
-        responses_data = []
-        for i in range(5):
-            response_data = {
-                "question": self.questions[i],
-                "response_time": self.response_times[i],
-                "response": self.responses[i],
-                "most_repeated_words": self.most_repeated_words_lists[i],
-                "filler_words_count": self.filler_words_counts[i]
-            }
-            responses_data.append(response_data)
-        return responses_data
+def save_to_array():
+        
 
 # read questions from a CSV file
 def read_questions_from_csv(file_path):
@@ -62,6 +54,7 @@ def listen_and_record_response():
         except sr.RequestError as e:
             print(f"Could not request results from Google Speech Recognition service; {e}")
             return ""
+
 
 csv_file_path = "questions.csv"
 all_questions = read_questions_from_csv(csv_file_path)
