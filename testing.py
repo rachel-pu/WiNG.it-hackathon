978ed0ebb25c9d09b730616236a598d98ecc86d1
xiaoguo-jia <xiaoguojia@ufl.edu>
2024-04-05 23:03:08 -0400

    testing completed

--- testing.py
+++ testing.py
@@ -5,14 +5,14 @@ r= sr.Recognizer()
 
 while True:
     # try:
-        with sr.Microphone() as source:
-            r.adjust_for_ambient_noise(source,duration=2)
-            print("say something")
-            audio = r.listen(source)
-            text = r.recognize_google(audio)
-            text = text.lower()
+    with sr.Microphone() as source:
+        r.adjust_for_ambient_noise(source,duration=2)
+        print("say something")
+        audio = r.listen(source)
+        text = r.recognize_google(audio)
+        text = text.lower()
 
-            print(f"recognized text: {text}")
+        print(f"recognized text: {text}")
 
     # except:
     #     print("you were trying to be funny")
