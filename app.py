f6288614b76d6480f713d6b631a992ace703940e
rachel-pu <rpu@ufl.edu>
2024-04-05 23:09:17 -0400

    created instructions page

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def home():
     return render_template("home.html")
 
+@app.route("/instructions")
+def instructions():
+    return render_template("instructions.html")
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
